6973cc4b62d9c2fa67704ef58b5e159cc7cb4342
Jesse Tomchak <jtomchak@gmail.com>
2021-10-26 13:47:39 -0700

    remove token

--- config/functions/responses/404.js
+++ config/functions/responses/404.js
@@ -1,10 +1,6 @@
 "use strict";
 
 module.exports = async ({ request, response }) => {
-  console.log(request);
-  if (request.url === "/loaderio-f1e146938aa192280c8acfdadeb827b5/") {
-    return (response.body = "loaderio-f1e146938aa192280c8acfdadeb827b5");
-  }
   return response.notFound("Nope");
   // return ctx.notFound('My custom message 404');
 };
