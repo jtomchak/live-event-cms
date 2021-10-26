d7f1f960b1248387132f438879af92e4accad618
Jesse Tomchak <jtomchak@gmail.com>
2021-10-26 13:12:01 -0700

    404 loader

--- config/functions/responses/404.js
+++ config/functions/responses/404.js
@@ -1,5 +1,10 @@
-'use strict';
+"use strict";
 
-module.exports = async (/* ctx */) => {
+module.exports = async ({ request, response }) => {
+  console.log(request);
+  if (request.url === "/loaderio-f1e146938aa192280c8acfdadeb827b5") {
+    return (response.status = 200);
+  }
+  return response.notFound("Nope");
   // return ctx.notFound('My custom message 404');
 };
