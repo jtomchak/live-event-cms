9e9a312a67b8d168935a6518ed56ba03c12b2627
Jesse Tomchak <jtomchak@gmail.com>
2021-10-26 13:27:46 -0700

    in the body

--- config/functions/responses/404.js
+++ config/functions/responses/404.js
@@ -3,7 +3,7 @@
 module.exports = async ({ request, response }) => {
   console.log(request);
   if (request.url === "/loaderio-f1e146938aa192280c8acfdadeb827b5/") {
-    return (response.status = 200);
+    return (response.body = "loaderio-f1e146938aa192280c8acfdadeb827b5");
   }
   return response.notFound("Nope");
   // return ctx.notFound('My custom message 404');
